2cc7e966e266426ecaebc7cb7d7088d4118a3c39
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-25 23:14:39 -0400

    added the import for Stopwatch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import './App.css';
-
+//This imports stopwatch which is later called via the Stopwatch class
+//psuedocode: call the Stopwatch in the App function to display it on the webpage
+import Stopwatch from './components/Stopwatch';
 function App() {
   return (
     <div className="App">
