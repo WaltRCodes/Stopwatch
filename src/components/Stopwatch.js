18ca8ebadf5c8544b95ea85e12c371b16cf5a6cc
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-25 22:35:49 -0400

    adde comments for the class

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,3 +1,5 @@
 //first I import React as it is needed for my code
 //psuedocode import react
 import React from 'react';
+//created the stopwatch class which extends component
+//psuedocode: create the Stopwatch class
